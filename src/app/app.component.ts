6fe776cde76207af091ebab0a35eb5ec66641a7c
oloza <olozra@gmail.com>
2024-02-08 12:13:11 -0400

    working->list,new,search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+// import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Customer, Representative } from './customer';
+import { CustomerService } from './customerservice';
+import { Table } from 'primeng/table';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,84 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  items: MenuItem[] | undefined;
   title = 'sysLnk';
+  value = 'hw';
+  // customers: any;
+  // loading: any;
+  nombreSistema: String;
+  dirUrl:String;
+  
+  customers!: Customer[];
+  selectedCustomers!: Customer[];
+  representatives!: Representative[];
+  statuses!: any[];
+  loading: boolean = true;
+
+  //@ViewChild('dt') table!: Table ;
+
+  @ViewChild('dt') dt: Table | undefined;
+
+  constructor(private customerService: CustomerService, private primengConfig: PrimeNGConfig) { }
+
+  
+  applyFilterGlobal($event: any, stringVal: any) {
+    this.dt!.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
+  }
+
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Inicio',
+        icon: 'pi pi-fw pi-plus',
+      },
+      {
+        label: 'Nuevo',
+        icon: 'pi pi-fw pi-plus',
+        command: (event) => {
+          //event.originalEvent: Browser event
+          //event.item: menuitem metadata
+          console.log('new event');
+          this.showDialog();
+        }
+      }
+    ];
+
+    this.customerService.getCustomersLarge().then(customers => {
+      this.customers = customers;
+      this.loading = false;
+  });
+
+  this.representatives = [
+      {name: "Amy Elsner", image: 'amyelsner.png'},
+      {name: "Anna Fali", image: 'annafali.png'},
+      {name: "Asiya Javayant", image: 'asiyajavayant.png'},
+      {name: "Bernardo Dominic", image: 'bernardodominic.png'},
+      {name: "Elwin Sharvill", image: 'elwinsharvill.png'},
+      {name: "Ioni Bowcher", image: 'ionibowcher.png'},
+      {name: "Ivan Magalhaes",image: 'ivanmagalhaes.png'},
+      {name: "Onyama Limba", image: 'onyamalimba.png'},
+      {name: "Stephen Shaw", image: 'stephenshaw.png'},
+      {name: "XuXue Feng", image: 'xuxuefeng.png'}
+  ];
+
+  this.statuses = [
+      {label: 'Unqualified', value: 'unqualified'},
+      {label: 'Qualified', value: 'qualified'},
+      {label: 'New', value: 'new'},
+      {label: 'Negotiation', value: 'negotiation'},
+      {label: 'Renewal', value: 'renewal'},
+      {label: 'Proposal', value: 'proposal'}
+  ]
+  this.primengConfig.ripple = true;
+
+  }
+
+  visible: boolean = false;
+
+  showDialog() {
+      this.visible = true;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,56 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { InputTextModule } from 'primeng/inputtext';
 import { AppComponent } from './app.component';
+ import { FormsModule } from '@angular/forms';
+import { MenubarModule } from 'primeng/menubar';
+//----
+// import { NgModule }      from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// import { AppComponent }   from './app.component';
+import { CustomerService } from './customerservice';
+
+import {TableModule} from 'primeng/table';
+import {CalendarModule} from 'primeng/calendar';
+import {SliderModule} from 'primeng/slider';
+import {DialogModule} from 'primeng/dialog';
+import {MultiSelectModule} from 'primeng/multiselect';
+import {ContextMenuModule} from 'primeng/contextmenu';
+import {ButtonModule} from 'primeng/button';
+import {ToastModule} from 'primeng/toast';
+// import {InputTextModule} from 'primeng/inputtext';
+import {ProgressBarModule} from 'primeng/progressbar';
+import {DropdownModule} from 'primeng/dropdown';
+import {StyleClassModule} from 'primeng/styleclass';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    FormsModule,
+    MenubarModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    TableModule,
+    CalendarModule,
+    SliderModule,
+    DialogModule,
+    MultiSelectModule,
+    ContextMenuModule,
+    ButtonModule,
+    ToastModule,
+    ProgressBarModule,
+    DropdownModule,
+    StyleClassModule
   ],
-  providers: [],
+  providers: [CustomerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
